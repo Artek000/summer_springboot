1fce7e30d460b3ecd7edf076188d65422c72778f
Artek000 <1111255555@mail.ru>
2022-07-15 00:38:28 +0300

    v1.2

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,60 @@
+const tovarListTemplate = '<div v-for="tovar in tovars" class="col tovar-list-item">\n' +
+    '            <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-4 shadow-lg">\n' +
+    '                <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">\n' +
+    '                    <h2 class="m-auto pb-5 fw-bold">{{tovar.name}}</h2>\n' +
+    '                    <ul class="d-flex list-unstyled mt-auto">\n' +
+    '                        <li class="me-auto">\n' +
+    '                            <small>{{tovar.price}} руб.</small>\n' +
+    '                        </li>\n' +
+    '                        <li class="d-flex align-items-center me-3">\n' +
+    '                            <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#geo-fill"></use></svg>\n' +
+    '                            <small class="fw-semibold">{{tovar.category}}</small>\n' +
+    '                        </li>\n' +
+    '                        <li class="d-flex align-items-center">\n' +
+    '                            <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#calendar3"></use></svg>\n' +
+    '                            <small>{{tovar.ostatok}} шт.</small>\n' +
+    '                        </li>\n' +
+    '                    </ul>\n' +
+    '                </div>\n' +
+    '            </div>\n' +
+    '        </div>';
+
+const TovarComponent = {
+    template: 'template-for-tovar',
+    props: {
+        name: {type: String, required: true},
+        category: {type: String, required: true},
+        price: {type: Number},
+        ostatok: {type: Number},
+        dostavka_price: {type: Number},
+        sale: {type: Number}
+    }
+}
+
+const TovarListComponent = {
+    components: {tovar: TovarComponent},
+    template: tovarListTemplate,
+    created: function (){
+        fetch("http://localhost:8080/tovars")
+            .then(response => response.json())
+            .then(data => data.forEach(tovar => this.tovars.push(tovar)))
+    },
+    data() {
+        return {
+            tovars: []
+            // tovars: [
+            //     {name: "Moloko", category: "Drink", price: "69", ostatok: "850", dostavka_price: "35", sale: "20"},
+            //     {name: "Banan", category: "Food", price: "87", ostatok: "430", dostavka_price: "6", sale: "5"},
+            //     {name: "CocaCola", category: "Drink", price: "88", ostatok: "1850", dostavka_price: "25", sale: "15"}
+            // ]
+        }
+    }
+}
+
+const app = Vue.createApp({
+    components: {
+        'tovar-list': TovarListComponent
+    }
+
+});
+app.mount("#app");
