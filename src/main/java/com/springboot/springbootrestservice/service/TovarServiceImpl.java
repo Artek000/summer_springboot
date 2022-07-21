4f226d26c917f4465215caab704b15746e469fd5
Artek000 <1111255555@mail.ru>
2022-07-22 02:25:38 +0300

    v1.5

--- src/main/java/com/springboot/springbootrestservice/service/TovarServiceImpl.java
+++ src/main/java/com/springboot/springbootrestservice/service/TovarServiceImpl.java
@@ -1,5 +1,8 @@
 package com.springboot.springbootrestservice.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
 import com.springboot.springbootrestservice.model.Tovar;
 import org.springframework.stereotype.Service;
 import org.yaml.snakeyaml.Yaml;
@@ -58,12 +61,22 @@ public class TovarServiceImpl implements TovarService {
 
     @Override
     public void save_tovars() {
-        readAll();
-        PrintWriter writer;
+        //readAll();
+       // final Map<String, Tovar> TOVAR_LIST = new HashMap<>();
+        //for(Tovar tovar: TOVAR_REPOSITORY_MAP.values()){}
+        //System.out.println(TOVAR_REPOSITORY_MAP.values());
+        //PrintWriter writer;
         try {
-            writer = new PrintWriter(new File("C:\\Users\\Артем\\Desktop\\summer practise\\Project\\src\\main\\resources\\static\\yml_data\\data.yml"));
-            Yaml yaml = new Yaml();
-            yaml.dump(TOVAR_REPOSITORY_MAP, writer);
+            //writer = new PrintWriter(new File("C:\\Users\\Артем\\Desktop\\summer practise\\Project\\src\\main\\resources\\static\\yml_data\\data.yml"));
+            //Yaml yaml = new Yaml();
+//            for ( Map.Entry<Integer, Tovar> entry : TOVAR_REPOSITORY_MAP.entrySet()) {
+//                Integer key = entry.getKey();
+//                Tovar tovar = entry.getValue();
+//
+//            }
+            //yaml.dump(readAll().get(1), writer);
+            ObjectMapper mapper = new ObjectMapper(new YAMLFactory().disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER));
+            mapper.writeValue(new File("C:\\Users\\Артем\\Desktop\\summer practise\\Project\\src\\main\\resources\\static\\yml_data\\data.yml"), readAll());
         }
         catch (Exception e){
             PrintStream ps;
